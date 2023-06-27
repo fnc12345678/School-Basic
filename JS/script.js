11150d0f5df8ef3aaa802750430dd50ff07d0487
fnc12345678 <137799279+fnc12345678@users.noreply.github.com>
2023-06-27 20:58:40 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,17 @@
 function copyContent() {
   // Seleziona l'elemento del titolo del primo contenitore
-  var titleElement = document.querySelector('.content:first-child h1');
+  var titleElement = document.querySelector('.content1 h1');
   // Seleziona l'elemento del contenuto del secondo contenitore
-  var contentElement = document.querySelector('.content:nth-child(2)');
+  var contentElement = document.querySelector('.content');
+
+  // Ottieni il titolo del primo contenitore e rimuovi il testo indesiderato
+  var titleText = titleElement.innerText.replace("If you want to copy the content of the lessons click on the button", "").trim();
 
   // Crea un elemento textarea
   var textarea = document.createElement('textarea');
 
-  // Rimuovi il testo indesiderato dal valore del titolo
-  var titleText = titleElement.textContent.replace("If you want to copy the content of the lessons click on the button", "");
-  // Imposta il valore del textarea con il testo del titolo e del contenuto
-  textarea.value = titleText + '\n\n' + contentElement.textContent;
+  // Imposta il valore del textarea con il titolo e il contenuto
+  textarea.value = titleText + '\n\n' + contentElement.innerText.trim();
 
   // Aggiungi il textarea al documento
   document.body.appendChild(textarea);
