9da544c3e8c162095b7369000f34231ccdf9707c
fnc12345678 <137799279+fnc12345678@users.noreply.github.com>
2023-06-27 20:47:24 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,31 @@
 function copyContent() {
+  // Seleziona l'elemento del titolo del primo contenitore
+  var titleElement = document.querySelector('.content:first-child h1');
+  // Seleziona l'elemento del contenuto del secondo contenitore
+  var contentElement = document.querySelector('.content:nth-child(2)');
+
+  // Crea un elemento textarea
+  var textarea = document.createElement('textarea');
+
+  // Imposta il valore del textarea con il testo del titolo e del contenuto
+  var textareaValue = titleElement.textContent + '\n\n' + contentElement.textContent;
+  // Rimuovi il testo indesiderato
+  textareaValue = textareaValue.replace("If you want to copy the content of the lessons click on the button", "");
+  textarea.value = textareaValue;
+
+  // Aggiungi il textarea al documento
+  document.body.appendChild(textarea);
+
+  // Seleziona il testo nel textarea
+  textarea.select();
+
+  // Copia il testo negli appunti del dispositivo
+  document.execCommand('copy');
+
+  // Rimuovi il textarea dal documento
+  document.body.removeChild(textarea);
+}
+function copyContent() {
       // Seleziona l'elemento con la classe "content"
       var content = document.querySelector('.content');
       
