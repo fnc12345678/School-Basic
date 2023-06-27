bf149a504d506db9cd80e27fc3881f13a2f009a6
fnc12345678 <137799279+fnc12345678@users.noreply.github.com>
2023-06-27 20:49:46 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -7,11 +7,10 @@ function copyContent() {
   // Crea un elemento textarea
   var textarea = document.createElement('textarea');
 
+  // Rimuovi il testo indesiderato dal valore del titolo
+  var titleText = titleElement.textContent.replace("If you want to copy the content of the lessons click on the button", "");
   // Imposta il valore del textarea con il testo del titolo e del contenuto
-  var textareaValue = titleElement.textContent + '\n\n' + contentElement.textContent;
-  // Rimuovi il testo indesiderato
-  textareaValue = textareaValue.replace("If you want to copy the content of the lessons click on the button", "");
-  textarea.value = textareaValue;
+  textarea.value = titleText + '\n\n' + contentElement.textContent;
 
   // Aggiungi il textarea al documento
   document.body.appendChild(textarea);
@@ -24,26 +23,4 @@ function copyContent() {
 
   // Rimuovi il textarea dal documento
   document.body.removeChild(textarea);
-}
-function copyContent() {
-      // Seleziona l'elemento con la classe "content"
-      var content = document.querySelector('.content');
-      
-      // Crea un elemento textarea
-      var textarea = document.createElement('textarea');
-      
-      // Imposta il valore del textarea come il testo della lezione
-      textarea.value = content.textContent;
-            
-      // Aggiungi il textarea al documento
-      document.body.appendChild(textarea);
-      
-      // Seleziona il testo nel textarea
-      textarea.select();
-      
-      // Copia il testo negli appunti del dispositivo
-      document.execCommand('copy');
-      
-      // Rimuovi il textarea dal documento
-      document.body.removeChild(textarea);
 }
