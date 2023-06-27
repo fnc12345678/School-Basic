d47ae2600935f608d594bd73fea4628b306ae0c9
fnc12345678 <137799279+fnc12345678@users.noreply.github.com>
2023-06-27 21:31:12 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -39,3 +39,20 @@ function copyContent() {
   // Rimuovi il textarea dal documento
   document.body.removeChild(textarea);
 }
+
+
+
+function toggleCategory(category) {
+  var categoryDivider = document.getElementById(category);
+  categoryDivider.classList.toggle("open");
+
+  var lessons = document.getElementsByClassName(category);
+  for (var i = 0; i < lessons.length; i++) {
+    lessons[i].classList.toggle("open");
+  }
+}
+
+function toggleAnswer(questionNumber) {
+  const answer = document.getElementById(`answer${questionNumber}`);
+  answer.style.display = answer.style.display === "block" ? "none" : "block";
+}
