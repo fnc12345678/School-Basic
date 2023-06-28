64029b10c29cd20d5f0f6c487a97941c5d978899
fnc12345678 <137799279+fnc12345678@users.noreply.github.com>
2023-06-28 21:01:08 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,47 +1,3 @@
-function copyContent() {
-  // Seleziona l'elemento del primo contenitore
-  var container1 = document.querySelector('.content1');
-
-  // Clona l'elemento del primo contenitore per conservare la struttura HTML
-  var container1Clone = container1.cloneNode(true);
-
-  // Rimuovi il testo indesiderato dal clone del primo contenitore
-  var unwantedText = container1Clone.querySelector('.button-text');
-  unwantedText.remove();
-
-  // Seleziona l'elemento del secondo contenitore
-  var container2 = document.querySelector('.content');
-
-  // Clona l'elemento del secondo contenitore per conservare la struttura HTML
-  var container2Clone = container2.cloneNode(true);
-
-  // Crea un elemento textarea
-  var textarea = document.createElement('textarea');
-
-  // Prendi il titolo dal primo contenitore
-  var title = container1Clone.querySelector('h1').textContent.trim();
-
-  // Prendi il contenuto dal secondo contenitore
-  var content = container2Clone.textContent.trim();
-
-  // Concatena il titolo e il contenuto nel textarea
-  textarea.value = title + '\n\n' + content;
-
-  // Aggiungi il textarea al documento
-  document.body.appendChild(textarea);
-
-  // Seleziona il testo nel textarea
-  textarea.select();
-
-  // Copia il testo negli appunti del dispositivo
-  document.execCommand('copy');
-
-  // Rimuovi il textarea dal documento
-  document.body.removeChild(textarea);
-}
-
-
-
 function toggleCategory(category) {
   var categoryDivider = document.getElementById(category);
   categoryDivider.classList.toggle("open");
@@ -58,24 +14,24 @@ function toggleAnswer(questionNumber) {
 }
 
 
-
-    function createSearchResultHTML(lessonTitle, lessonContent, lessonLink, lessonImage) {
-      return `
-        <div class="result">
-          <a href="${lessonLink}" class="lesson-link">
-            <img src="${lessonImage}" alt="...">
-            <h3>${lessonTitle}</h3>
-            <div class="lesson-content">
-              ${lessonContent}
-            </div>
-          </a>
+function createSearchResultHTML(lessonTitle, lessonContent, lessonLink, lessonImage) {
+  return `
+  <div class="result">
+  <div id="searchResultContainer" class="result-container"></div>
+      <a href="${lessonLink}" class="lesson-link">
+        <img src="${lessonImage}" alt="...">
+        <h3>${lessonTitle}</h3>
+        <div class="lesson-content">
+          ${lessonContent}
         </div>
+      </a>
+    </div>
+  </div>
+  `;
+}
 
-      `;
-    }
 
- 
-    function performSearch() {
+function performSearch() {
   var searchTerm = document.getElementById("searchInput").value.trim().toLowerCase();
   var lessons = document.getElementsByClassName("lesson");
   var resultContainer = document.getElementById("searchResultContainer");
@@ -91,8 +47,8 @@ function toggleAnswer(questionNumber) {
   } else {
     for (var i = 0; i < lessons.length; i++) {
       var lesson = lessons[i];
-      var lessonTitle = lesson.getElementsByTagName("h3")[0].textContent;
-      var lessonContent = lesson.getElementsByClassName("lesson-content")[0].innerHTML;
+      var lessonTitle = lesson.getElementsByTagName("h3")[0].textContent.toLowerCase();
+      var lessonContent = lesson.getElementsByClassName("lesson-content")[0].innerHTML.toLowerCase();
       var lessonLink = lesson.getElementsByTagName("a")[0].getAttribute("href");
       var lessonImage = lesson.getElementsByTagName("img")[0].getAttribute("src");
 
@@ -100,16 +56,15 @@ function toggleAnswer(questionNumber) {
         resultContainer.innerHTML += createSearchResultHTML(lessonTitle, lessonContent, lessonLink, lessonImage);
         hasResults = true;
       }
-      if (hasResults) {
-      resultContainer.style.display = "block";
-      noResultsMessage.style.display = "none";
-    } else {
-      resultContainer.style.display = "none";
-      noResultsMessage.style.display = "block";
-      window.location.href = "404.html";
+      
     }
-    }
-
-    
+  }
+  if (hasResults) {
+    resultContainer.style.display = "block";
+    noResultsMessage.style.display = "none";
+  } else {
+    resultContainer.style.display = "none";
+    noResultsMessage.style.display = "block";
+    window.location.href = "404.html";
   }
 }
