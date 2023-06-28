a61d5cb071fe7128d9d17b83613c05ad751c0037
fnc12345678 <137799279+fnc12345678@users.noreply.github.com>
2023-06-28 22:15:46 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -13,6 +13,64 @@ function toggleAnswer(questionNumber) {
   answer.style.display = answer.style.display === "block" ? "none" : "block";
 }
 
+
+  function copyContent() {
+  var container1 = document.querySelector('.content1');
+  var container1Clone = container1.cloneNode(true);
+  var unwantedText = container1Clone.querySelector('.button-text');
+  unwantedText.remove();
+  var title = container1Clone.querySelector('h1').textContent.trim();
+
+  // Seleziona gli elementi con la classe "content"
+  var containers = document.querySelectorAll('.content');
+
+  // Crea un array per contenere i cloni degli elementi
+  var containerClones = [];
+
+  // Itera su ogni elemento e clonalo
+  for (var i = 0; i < containers.length; i++) {
+    var container = containers[i];
+    var containerClone = container.cloneNode(true);
+    containerClones.push(containerClone);
+  }
+
+  // Crea un elemento textarea
+  var textarea = document.createElement('textarea');
+
+  // Concatena il titolo e il testo di tutti i cloni degli elementi nel textarea
+  var content = title + '\n\n';
+  for (var i = 0; i < containerClones.length; i++) {
+    var containerClone = containerClones[i];
+    content += containerClone.textContent.trim() + '\n\n';
+  }
+
+  // Rimuovi l'ultimo doppio spazio vuoto
+  content = content.trim();
+
+  // Aggiungi il contenuto al textarea
+  textarea.value = content;
+
+  // Aggiungi il textarea al documento
+  document.body.appendChild(textarea);
+
+  // Seleziona il testo nel textarea
+  textarea.select();
+
+  // Copia il testo negli appunti del dispositivo
+  navigator.clipboard.writeText(textarea.value)
+    .then(function() {
+      console.log('Testo copiato negli appunti!');
+      // Rimuovi il textarea dal documento
+      document.body.removeChild(textarea);
+    })
+    .catch(function(error) {
+      console.error('Errore durante la copia del testo negli appunti:', error);
+      // Rimuovi il textarea dal documento anche in caso di errore
+      document.body.removeChild(textarea);
+    });
+}
+
+
 function createSearchResultHTML(lessonTitle, lessonContent, lessonLink, lessonImage) {
   return `
   <div class="result">
